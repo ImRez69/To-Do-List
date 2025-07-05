8eb0bb8f5f6e646c66eb8671dbae96e9b57ff017
ImRez69 <CaptianAlirez@gmail.com>
2025-07-06 00:37:27 +0330

    Update

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 const themeBtn = document.getElementById("theme-btn");
 let lightTheme = document.body.classList.contains("light");
+themeBtn.addEventListener(
+  "click",
+  (toggleTheme = () => {
+    if (lightTheme) {
+      themeBtn.innerText = "🌙";
+      document.body.classList.replace("light", "dark");
+      lightTheme = false;
+    } else {
+      themeBtn.innerText = "☀️";
+      document.body.classList.replace("dark", "light");
+      lightTheme = true;
+    }
+    localStorage.setItem("Light Theme", `${lightTheme}`);
+  })
+);
 
 (function currentTheme() {
   if (localStorage.getItem("Light Theme") === "true") {
@@ -13,15 +28,15 @@ let lightTheme = document.body.classList.contains("light");
   }
 })();
 
-themeBtn.addEventListener("click", toggleTheme = () => {
-  if (lightTheme) {
-    themeBtn.innerText = "🌙";
-    document.body.classList.replace("light", "dark");
-    lightTheme = false;
-  } else {
-    themeBtn.innerText = "☀️";
-    document.body.classList.replace("dark", "light");
-    lightTheme = true;
-  }
-  localStorage.setItem("Light Theme", `${lightTheme}`);
-});
+function itemAction() {
+  const itemForm = document.getElementById("item-form");
+  const formInput = document.getElementById("form-input");
+  const addItem = (e) => {
+    e.preventDefault();
+    const newItem = formInput.value;
+    const li = document.createElement("li")
+    
+  };
+  itemForm.addEventListener("submit", addItem);
+}
+itemAction()
