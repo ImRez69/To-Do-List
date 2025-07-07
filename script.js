37e5020ff0312fcf0c17acf6a3559f45747edfde
ImRez69 <CaptianAlirez@gmail.com>
2025-07-07 09:35:45 +0330

    Change Icon To CSS Style & Add Remove Item

--- script.js
+++ script.js
@@ -29,17 +29,19 @@ themeBtn.addEventListener(
 })();
 
 function itemAction() {
+  // Add Item
   const itemForm = document.getElementById("item-form");
   const formInput = document.getElementById("form-input");
   const addItem = (e) => {
     e.preventDefault();
     const newItem = formInput.value;
     if (newItem === "") {
-      document.getElementById("epmty-input-error").classList.add("show-error")
-      return
-    }
-    else{
-      document.getElementById("epmty-input-error").classList.remove("show-error")
+      document.getElementById("epmty-input-error").classList.add("show-error");
+      return;
+    } else {
+      document
+        .getElementById("epmty-input-error")
+        .classList.remove("show-error");
     }
 
     const li = document.createElement("li");
@@ -49,25 +51,19 @@ function itemAction() {
     span.innerText = newItem;
     li.appendChild(span);
 
-    const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-    svg.classList.add("icon");
-    svg.classList.add("add-icon");
-    svg.setAttribute("clip-rule", "evenodd");
-    svg.setAttribute("fill-rule", "evenodd");
-    svg.setAttribute("stroke-linejoin", "round");
-    svg.setAttribute("stroke-miterlimit", "2");
-    svg.setAttribute("viewBox", "0 0 24 24");
-    svg.setAttribute("xmlns", "http://www.w3.org/2000/svg");
-    svg.innerHTML =
-      '<path d="m11 11h-7.25c-.414 0-.75.336-.75.75s.336.75.75.75h7.25v7.25c0 .414.336.75.75.75s.75-.336.75-.75v-7.25h7.25c.414 0 .75-.336.75-.75s-.336-.75-.75-.75h-7.25v-7.25c0-.414-.336-.75-.75-.75s-.75.336-.75.75z" fill-rule="nonzero"/>';
-    li.appendChild(svg);
+    const div = document.createElement("div");
+    div.classList.add("remove-icon");
+    li.appendChild(div);
 
     const itemsList = document.getElementById("items-list");
     itemsList.appendChild(li);
   };
   itemForm.addEventListener("submit", addItem);
+
+  // Remove Item
+  document.querySelectorAll(".remove-icon").forEach((icon) => icon.addEventListener("click", (e) => e.target.parentElement.remove()));
 }
 itemAction();
 
-const test = ["ali","arash","mehdi","mamad"]
-localStorage.setItem("names",JSON.stringify(test));
+// const test = ["ali","arash","mehdi","mamad"]
+// localStorage.setItem("names",JSON.stringify(test));
