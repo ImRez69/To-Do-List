32591766462e152388513f8c22fae623dd462c79
ImRez69 <CaptianAlirez@gmail.com>
2025-07-12 13:13:15 +0330

    Add Clock

--- script.js
+++ script.js
@@ -28,6 +28,14 @@ themeBtn.addEventListener(
   }
 })();
 
+// Clock
+setInterval(()=>{
+  const now = new Date();
+  const time = now.toLocaleTimeString();
+  document.getElementById("clock").innerText = time;
+  
+},1000)
+
 function itemAction() {
   // Items From Local Storage
   let localStorageItemsArray = [];
