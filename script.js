f28d57602b0d425246f2e59933fe89cefb485177
ImRez69 <CaptianAlirez@gmail.com>
2025-07-26 09:40:49 +0330

    Add Prevent Default For Submit Button

--- script.js
+++ script.js
@@ -1,18 +1,7 @@
-const themeBtn = document.getElementById("theme-btn");
 let lightTheme = document.body.classList.contains("light");
-const toggleTheme = () => {
-  if (lightTheme) {
-    themeBtn.innerText = "🌙";
-    document.body.classList.replace("light", "dark");
-    lightTheme = false;
-  } else {
-    themeBtn.innerText = "☀️";
-    document.body.classList.replace("dark", "light");
-    lightTheme = true;
-  }
-  localStorage.setItem("Light Theme", `${lightTheme}`);
-};
-themeBtn.addEventListener("click", toggleTheme);
+const themeBtn = document.getElementById("theme-btn");
+const itemForm = document.getElementById("item-form");
+const addItemBtn = document.getElementById("add-item-btn");
 
 (function currentStatus() {
   // Current Theme
@@ -34,12 +23,33 @@ themeBtn.addEventListener("click", toggleTheme);
   document.getElementById("date").innerText = new Date().toLocaleDateString();
 })();
 
-function checkUi() {
-  if (document.querySelectorAll("ul li span").length <= 0) {
-    document.getElementById("clear-btn").style.display = "none";
-    document.getElementById("filter-item").style.display = "none";
+function toggleTheme(){
+  if (lightTheme) {
+    themeBtn.innerText = "🌙";
+    document.body.classList.replace("light", "dark");
+    lightTheme = false;
   } else {
-    document.getElementById("clear-btn").style.display = "";
-    document.getElementById("filter-item").style.display = "";
+    themeBtn.innerText = "☀️";
+    document.body.classList.replace("dark", "light");
+    lightTheme = true;
   }
+  localStorage.setItem("Light Theme", `${lightTheme}`);
+};
+
+function addItem(e){
+  e.preventDefault()
 }
+
+// function checkUi() {
+//   if (document.querySelectorAll("ul li span").length <= 0) {
+//     document.getElementById("clear-btn").style.display = "none";
+//     document.getElementById("filter-item").style.display = "none";
+//   } else {
+//     document.getElementById("clear-btn").style.display = "";
+//     document.getElementById("filter-item").style.display = "";
+//   }
+// }
+
+// Event Listeners
+themeBtn.addEventListener("click", toggleTheme);
+itemForm.addEventListener("submit", addItem);
